7e30dc6707b4e3a090545f8c4d431b88aadbbd75
Krzysztof Mazur <ruzam.pl@gmail.com>
2021-05-04 12:02:21 +0200

    New configuration tree builder

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,10 +14,9 @@ use LSB\PricelistBundle\Manager\PricelistManager;
 use LSB\PricelistBundle\Manager\PricelistPositionManager;
 use LSB\PricelistBundle\Repository\PricelistPositionRepository;
 use LSB\PricelistBundle\Repository\PricelistRepository;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use LSB\UtilityBundle\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use LSB\UtilityBundle\DependencyInjection\BaseExtension as BE;
-
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -48,41 +47,27 @@ class Configuration implements ConfigurationInterface
             ->arrayNode(BE::CONFIG_KEY_RESOURCES)
             ->children()
 
-            // Start Pricelist
-            ->arrayNode('pricelist')
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->arrayNode(BE::CONFIG_KEY_CLASSES)
-            ->children()
-            ->scalarNode(BE::CONFIG_KEY_ENTITY)->end()
-            ->scalarNode(BE::CONFIG_KEY_INTERFACE)->defaultValue(PricelistInterface::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FACTORY)->defaultValue(PricelistFactory::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_REPOSITORY)->defaultValue(PricelistRepository::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_MANAGER)->defaultValue(PricelistManager::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FORM)->defaultValue(PricelistType::class)->end()
+            //Pricelist
+            ->resourceNode(
+                'pricelist',
+                PricelistInterface::class,
+                PricelistFactory::class,
+                PricelistRepository::class,
+                PricelistManager::class,
+                PricelistType::class
+            )
             ->end()
-            ->end()
-            ->end()
-            ->end()
-            // End Pricelist
 
-            // Start PricelistPosition
-            ->arrayNode('pricelist_position')
-            ->addDefaultsIfNotSet()
-            ->children()
-            ->arrayNode(BE::CONFIG_KEY_CLASSES)
-            ->children()
-            ->scalarNode(BE::CONFIG_KEY_ENTITY)->end()
-            ->scalarNode(BE::CONFIG_KEY_INTERFACE)->defaultValue(PricelistPositionInterface::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FACTORY)->defaultValue(PricelistPositionFactory::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_REPOSITORY)->defaultValue(PricelistPositionRepository::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_MANAGER)->defaultValue(PricelistPositionManager::class)->end()
-            ->scalarNode(BE::CONFIG_KEY_FORM)->defaultValue(PricelistPositionType::class)->end()
+            //Pricelist position
+            ->resourceNode(
+                'pricelist_position',
+                PricelistPositionInterface::class,
+                PricelistPositionFactory::class,
+                PricelistPositionRepository::class,
+                PricelistPositionManager::class,
+                PricelistPositionType::class
+            )
             ->end()
-            ->end()
-            ->end()
-            ->end()
-            // End PricelistPosition
 
             ->end()
             ->end()
